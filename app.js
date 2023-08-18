db451c9b4a4d0e480d42adc6afb21c9d4c52b284
Digvijay Mane <digvijaymane2003@gmail.com>
2023-08-18 20:03:46 +0530

    experiment 1

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const { error } = require("console");
 const express = require("express");
+const mysql = require('mysql2');
 const path = require("path");
 const app = express();
 const port = 80;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Digvijay19$',
+    database: 'project'
+});
+
 
 // For using static files
 app.use("/static", express.static("static"));
@@ -11,7 +21,7 @@ app.use(express.urlencoded());
 // app.set("view engine", "html");
 
 //set view directory
-// app.set("view", path.join(__dirname, "view"));
+app.set("view", path.join(__dirname, "view"));
 
 app.get("/", (req, res) => {
     res.status(200).sendFile(path.join(__dirname, "view/index.html"));
@@ -46,6 +56,23 @@ app.get("/testimonial", (req, res) => {
 // app.post("/about", (req, res) => {
 //     res.send("This is my post requist first about of express app")
 // });
+
+app.post('/contact', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var Contant_number = req.body.Contant_number;
+    var message = req.body.message;
+    console.log("name");
+    connection.connect((error) => {
+        if (error) throw error;
+        var sql = "INSERT INTO customer values('" + name + "','" + email + "','" + Contant_number + "','" + message + "')"
+        connection.query(sql, function (error, result) {
+            if (error) throw error;
+            res.send('Customer have been registered successfully' + result.insertId);
+        })
+    })
+})
+
 app.listen(port, () => {
     console.log(`this application started successfully on port ${port}`);
 });
